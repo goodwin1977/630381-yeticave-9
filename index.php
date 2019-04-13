bb192c4287d3a3cab11ff5b4a29126366e26b9f8
goodwin1977 <maxim.busynkin@gmail.com>
2019-04-14 00:17:31 +0500

    реализован вывод лотов

--- index.php
+++ index.php
@@ -102,24 +102,26 @@ $i = 0; // счетчик цикла
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <?php foreach($lots as $k => $v): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$v['url'];?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$v['cat'];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$v['lot_title'];?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$v['price'];?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <? endforeach; ?>
         </ul>
     </section>
 </main>
