815e565abbb8e2326706f157b34b8bc9d21c2742
goodwin1977 <maxim.busynkin@gmail.com>
2019-04-21 12:31:28 +0500

    поправил функцию форматирования стоимости в выводе лотов

--- index.php
+++ index.php
@@ -120,25 +120,26 @@ function format_price($price_float)
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <!--  -->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=format_price(123);?><b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <?php foreach($lots as $k => $v): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$v['url'];?>" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$v['cat'];?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$v['lot_title'];?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=format_price($v['price']);?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
                 </li>
-            
+            <? endforeach; ?>            
         </ul>
     </section>
 </main>
