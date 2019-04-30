b3504b2ffa0d253fe0530ed4f9e7c6bf6743fe04
goodwin1977 <maxim.busynkin@gmail.com>
2019-04-30 11:59:36 +0500

    правки в БД

--- schema.sql
+++ schema.sql
@@ -7,42 +7,56 @@ CREATE DATABASE yeticave
 -- Таблица категорий
 
 CREATE TABLE categories (
-  id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  name  VARCHAR(50) UNIQUE                      NOT NULL
+  id TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY   NOT NULL,
+  name VARCHAR(128) UNIQUE                         NOT NULL,
+  code VARCHAR(64) UNIQUE                          NOT NULL
 );
 
 -- таблица пользователей
 
 CREATE TABLE users (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY    NOT NULL,
-  reg_date TIMESTAMP DEFAULT NOW()              NOT NULL,
-  email VARCHAR(80) UNIQUE                      NOT NULL,
-  name VARCHAR(80)                              NOT NULL,
-  password VARCHAR(120)                         NOT NULL,
-  avatar VARCHAR(255),
-  contacts VARCHAR(255)
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+  email VARCHAR(256) UNIQUE                     NOT NULL,
+  name VARCHAR(128)                             NOT NULL,
+  password VARCHAR(128)                         NOT NULL,
+  avatar VARCHAR(512),
+  contacts VARCHAR(256)
 );
 
 -- таблица лотов
 
 CREATE TABLE lots (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY    NOT NULL,
-  creation_date TIMESTAMP DEFAULT NOW()         NOT NULL,
-  title VARCHAR(100)                            NOT NULL,
-  description VARCHAR(1000)                     NOT NULL,
-  image VARCHAR(100)                            NOT NULL,
-  start_price INT(10) UNSIGNED                  NOT NULL,
-  finish_date TIMESTAMP DEFAULT NOW()           NOT NULL,
+  creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  title VARCHAR(256)                            NOT NULL,
+  description text                              NOT NULL,
+  image_url VARCHAR(512)                        NOT NULL,
+  start_price INT UNSIGNED                      NOT NULL,
+  finish_date TIMESTAMP                         NOT NULL,
   bet_step INT UNSIGNED                         NOT NULL,
+  owner_id INT UNSIGNED                         NOT NULL,
+  winner_id INT UNSIGNED,
+  category_id INT UNSIGNED                      NOT NULL,
+  FOREIGN KEY (owner_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  INDEX lot_index (name, category_id)
 );
 
 -- таблица ставок
 
 CREATE TABLE bets (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY    NOT NULL,
-  bet_date TIMESTAMP DEFAULT NOW()              NOT NULL
+  bet_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP  NOT NULL,
+  cost INT UNSIGNED NOT NULL,
+  user_id INT UNSIGNED NOT NULL,
+  lot_id INT UNSIGNED NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (lot_id) REFERENCES lots(id),
+  INDEX user_lot (user_id, lot_id)
 );
 
-CREATE INDEX category_name ON categories(name);
+
 CREATE INDEX lot_name ON lots(title);
 CREATE INDEX user_email ON users(email);
