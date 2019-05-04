0a974c7305f183fbf7781b9e3db5da37dc534185
goodwin1977 <maxim.busynkin@gmail.com>
2019-05-04 23:02:13 +0500

    запись в таблицу категорий и юзеров

---
+++ queries.sql
@@ -0,0 +1,26 @@
+INSERT INTO categories (name, code) VALUES
+  ('Доски и лыжи', 'boards'),
+  ('Крепления', 'attachment'),
+  ('Ботинки', 'boots'),
+  ('Одежда', 'clothing'),
+  ('Инструменты', 'tools'),
+  ('Разное', 'other');
+
+INSERT INTO users (email, name, password, avatar, contacts) VALUES
+  ('appolon@mail.ru',
+   'Апполинарий',
+   'qwertyuiop',
+   'img/avatar1.jpg',
+   '+79122403330'),
+
+  ('leo@ya.ru',
+   'Леопольд',
+   'asdfghjkl',
+   'img/avatar2.jpg',
+   '+79998765432'),
+
+  ('sima@mail.ru',
+   'Серафима',
+   'zxcvbnmzxcvb',
+   'img/avatar3.jpg',
+   '+79122403816');
