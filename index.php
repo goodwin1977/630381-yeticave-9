14a04884bf6abd87e27625e8b6ec1ee19e511fbb
goodwin1977 <maxim.busynkin@gmail.com>
2019-05-08 00:44:15 +0500

    подключение и вывод лотов

--- index.php
+++ index.php
@@ -81,25 +81,41 @@ $link = mysqli_connect('localhost', 'root', '', 'yeticave');
 if ($link == false) {
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
 } else {
+
     mysqli_set_charset($link, "utf8");
+
     $get_categories_sql = 'SELECT * FROM categories';
     $get_categories = mysqli_query($link, $get_categories_sql);
     $categories = mysqli_fetch_all($get_categories, MYSQLI_ASSOC);
 
+    $get_lots_sql =
+        'SELECT
+            lots.id,
+            lots.title as lot_title,
+            lots.start_price,
+            lots.image_url,
+            lots.finish_date,
+            categories.name as category_name
+        FROM lots
+        JOIN categories ON categories.id = lots.id
+        ORDER BY lots.finish_date DESC;';
+    $get_lots = mysqli_query($link, $get_lots_sql);
+    $lots = mysqli_fetch_all($get_lots, MYSQLI_ASSOC);
 
 
-$page_content = include_template('index.php', [
-    'lots' => $lots,
-    'categories' => $categories
+
+    $page_content = include_template('index.php', [
+        'lots' => $lots,
+        'categories' => $categories
+        ]);
+    $layout_content = include_template('layout.php', [
+        'content' => $page_content,
+        'categories' => $categories,
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'title' => 'YetiCave - Главная страница'
     ]);
-$layout_content = include_template('layout.php', [
-	'content' => $page_content,
-    'categories' => $categories,
-    'is_auth' => $is_auth,
-    'user_name' => $user_name,
-	'title' => 'YetiCave - Главная страница'
-]);
 
-print($layout_content);
+    print($layout_content);
 }
 ?>
--- templates/index.php
+++ templates/index.php
@@ -20,19 +20,19 @@
             <?php foreach($lots as $k => $v): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$v['url'];?>" width="350" height="260" alt="">
+                        <img src="<?=$v['image_url'];?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$v['cat'];?></span>
+                        <span class="lot__category"><?=$v['category_name'];?></span>
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$v['lot_title'];?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=format_price($v['price']);?></span>
+                                <span class="lot__cost"><?=format_price($v['start_price']);?></span>
                             </div>
                             <!-- <div class="lot__timer timer"> -->
-                            <div class="lot__timer timer <?=is_last_hour('tomorrow') ? "timer--finishing" : '' ?>">
-                                    <?=get_time_to_lot("tomorrow") ?>
+                            <div class="lot__timer timer <?=is_last_hour($v['finish_date']) ? "timer--finishing" : '' ?>">
+                                    <?=get_time_to_lot($v['finish_date']) ?>
                             </div>
                             <!-- </div> -->
                         </div>
